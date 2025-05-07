315c79893656b3c34192424529283d726c627a48
ghghtls <ghghtls2@gmail.com>
2025-05-08 02:15:37 +0900

    feat: dotenv 환경변수 기능 추가 및 assets 등록, 다크테마 추가

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
-import 'pages/homepage/home_page.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_movie_info/pages/homepage/home_page.dart';
 
-Future<void> main() async {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
   await dotenv.load(fileName: ".env");
-  runApp(const MyApp());
+
+  runApp(const ProviderScope(child: MyApp()));
 }
 
 class MyApp extends StatelessWidget {
@@ -12,6 +15,19 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(title: 'TMDB App', home: const HomePage());
+    return MaterialApp(
+      title: 'Flutter Movie Info',
+      themeMode: ThemeMode.dark, // 다크테마를 기본으로 설정
+      theme: ThemeData.light(), // 필요하면 커스터마이즈
+      darkTheme: ThemeData.dark().copyWith(
+        scaffoldBackgroundColor: Colors.black,
+        appBarTheme: const AppBarTheme(
+          backgroundColor: Colors.black,
+          foregroundColor: Colors.white,
+        ),
+        textTheme: const TextTheme(bodyMedium: TextStyle(color: Colors.white)),
+      ),
+      home: const HomePage(),
+    );
   }
 }
